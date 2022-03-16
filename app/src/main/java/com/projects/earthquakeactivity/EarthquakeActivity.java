7a31f15bc2cccd2debfeb894d2a329e631169149
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2022-03-16 20:52:49 +0200

    updates

--- app/src/main/java/com/projects/earthquakeactivity/EarthquakeActivity.java
+++ app/src/main/java/com/projects/earthquakeactivity/EarthquakeActivity.java
@@ -1,40 +1,25 @@
 package com.projects.earthquakeactivity;
 
-import static androidx.core.content.PackageManagerCompat.LOG_TAG;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.projects.earthquakeactivity.Model.Earthquake;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
-    public final String DATA_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
+    public  final String DATA_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=25";
 
     public final static String LOG_TAG = "EQA_RES";
 
@@ -42,14 +27,13 @@ public class EarthquakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-
-        //ArrayList<Earthquake> earthquakes = QueryUtils.parseJSON(QueryUtils.JSON_DATA);
-        EarthquakeAsyncTask asyncTask = new EarthquakeAsyncTask();
-        asyncTask.execute();
+        Bundle bundle = new Bundle();
+        bundle.putString("URL", DATA_URL);
+        getSupportLoaderManager().initLoader(1, bundle, this).forceLoad();
 
     }
 
-    public void updateUI(ArrayList<Earthquake> earthquakes){
+    public void updateUI(ArrayList<Earthquake> earthquakes) {
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
@@ -57,93 +41,52 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakeListView.setAdapter(adapter);
     }
 
-    class EarthquakeAsyncTask extends AsyncTask<URL, Void, ArrayList<Earthquake>> {
+    @NonNull
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int id, @Nullable Bundle args) {
+        String url=null;
+        if(args!=null){
+            url = args.getString("URL");
 
-        @Override
-        protected ArrayList<Earthquake> doInBackground(URL... urls) {
-            URL url = createURL(DATA_URL);
-            String jsonResponse = "";
-            try {
-                jsonResponse = makeHttpRequest(url);
-            }catch (Exception e){
-                Log.v(LOG_TAG, "Problem making the HTTP request.", e);
-                e.printStackTrace();
-            }
-
-            return QueryUtils.parseJSON(jsonResponse);
         }
-
-        @Override
-        protected void onPostExecute(ArrayList<Earthquake> earthquakes) {
-            if(earthquakes == null){
-                return;
-            }
-            updateUI(earthquakes);
-        }
-
-        private URL createURL(String data_url) {
-            URL url = null;
-            try {
-                url = new URL(data_url);
-            } catch (MalformedURLException exception) {
-                Log.v(LOG_TAG, "Error with creating URL", exception);
-                return null;
-            }
-            return url;
-        }
-
-        private String makeHttpRequest(URL url) throws IOException {
-            String jsonResponse = "";
-
-            // If the URL is null, then return early.
-            if (url == null) {
-                return jsonResponse;
-            }
-
-            HttpURLConnection urlConnection = null;
-            InputStream inputStream = null;
-            try {
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setReadTimeout(10000);
-                urlConnection.setConnectTimeout(15000);
-                urlConnection.connect();
-
-                if (urlConnection.getResponseCode() == 200) {
-                    inputStream = urlConnection.getInputStream();
-                    jsonResponse = readFromStream(inputStream);
-                } else {
-                    Log.v(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
-                }
-            } catch (IOException e) {
-                Log.v(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (inputStream != null) {
-                    // function must handle java.io.IOException here
-                    inputStream.close();
-                }
-            }
-            return jsonResponse;
-        }
-
-        private String readFromStream(InputStream inputStream) throws IOException {
-            StringBuilder result = new StringBuilder();
-            if (inputStream != null) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String line = bufferedReader.readLine();
-                while (line != null) {
-                    result.append(line);
-                    line = bufferedReader.readLine();
-                }
-            }
-            return result.toString();
-        }
-
+        return new EarthquakeAsyncTaskLoader(this,url);
     }
 
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        if (data != null) {
+            updateUI((ArrayList<Earthquake>) data);
+        }
+    }
 
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Earthquake>> loader) {
+
+    }
+}
+
+class EarthquakeAsyncTaskLoader extends AsyncTaskLoader<List<Earthquake>> {
+    private String url;
+
+    public EarthquakeAsyncTaskLoader(@NonNull Context context,String url) {
+        super(context);
+        this.url = url;
+    }
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+    @Nullable
+    @Override
+    public List<Earthquake> loadInBackground() {
+        List<Earthquake> list = null;
+        try {
+            URL url = QueryUtils.createURL(this.url);
+            String response = QueryUtils.makeHttpRequest(url);
+            list = QueryUtils.parseJSON(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
--- app/src/main/java/com/projects/earthquakeactivity/QueryUtils.java
+++ app/src/main/java/com/projects/earthquakeactivity/QueryUtils.java
@@ -1,11 +1,23 @@
 package com.projects.earthquakeactivity;
 
+import static com.projects.earthquakeactivity.EarthquakeActivity.LOG_TAG;
+
+import android.util.Log;
+
 import com.projects.earthquakeactivity.Model.Earthquake;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,6 +47,68 @@ public class QueryUtils {
         }
 
         return earthquakes;
+    }
+
+    public static URL createURL(String data_url) {
+        URL url = null;
+        try {
+            url = new URL(data_url);
+        } catch (MalformedURLException exception) {
+            Log.v(LOG_TAG, "Error with creating URL", exception);
+            return null;
+        }
+        return url;
+    }
+
+    public static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.v(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.v(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    public static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder result = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                result.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        return result.toString();
     }
 
 
